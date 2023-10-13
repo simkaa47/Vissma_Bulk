2076d0c7175ed079b904dc78e54bb35d9ac45553
Serafim Prilezhaev <simkaa7@gmail.com>
2023-10-13 09:45:14 +0300

    A window for istiratel was completed

--- View/Windows/IstiratelSettingsWindow.axaml.cs
+++ View/Windows/IstiratelSettingsWindow.axaml.cs
@@ -1,6 +1,7 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
+using System;
 
 namespace View.Windows;
 
@@ -9,5 +10,39 @@ public partial class IstiratelSettingsWindow : Window
     public IstiratelSettingsWindow()
     {
         InitializeComponent();
+        this.Opened += OnOpened;
+    }
+
+    private void OnOpened(object? sender, EventArgs e)
+    {
+        this.WindowState = WindowState.Maximized;
+    }
+
+    private void OpenControlWindow(object? sender, RoutedEventArgs args)
+    {
+        if (App.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var current = desktop.MainWindow;
+            desktop.MainWindow = new ControlWindow
+            {
+                DataContext = this.DataContext
+            };
+            desktop.MainWindow.Show();
+            current.Close();
+        }
+    }
+
+    private void OpenErrorsWindow(object? sender, RoutedEventArgs args)
+    {
+        if (App.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var current = desktop.MainWindow;
+            desktop.MainWindow = new ErrorsWindow
+            {
+                DataContext = this.DataContext
+            };
+            desktop.MainWindow.Show();
+            current.Close();
+        }
     }
 }
