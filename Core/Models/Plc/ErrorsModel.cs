b37d619fb16ddaafa814c5292091491c2ff96b4b
Serafim Prilezhaev <simkaa7@gmail.com>
2024-03-05 11:27:41 +0300

    Добавлена ошибка конвеера

--- Core/Models/Plc/ErrorsModel.cs
+++ Core/Models/Plc/ErrorsModel.cs
@@ -72,7 +72,8 @@
                 new Parameter<bool>("ErrSqIstiratel2",              "Истиратель: аварийный концевик 2",                     false, true, 205, 9),
                 new Parameter<bool>("ErrDrobilkaDrive",             "Дробилка: сработало тепловое реле",                    false, true, 205, 10),
                 new Parameter<bool>("ErrIstiratelDrive",            "Истиратель: сработало тепловое реле",                  false, true, 205, 11),
-                new Parameter<bool>("ErrDryUnitDrive",              "Блок сушки: сработало тепловое реле",                  false, true, 205, 12)
+                new Parameter<bool>("ErrDryUnitDrive",              "Блок сушки: сработало тепловое реле",                  false, true, 205, 12),
+                new Parameter<bool>("ErrKonveyor39",                "Нет готовности от конвеера 39",                        false, true, 205, 13)
 
             };
         }
--- View/Converters/GetFromCollectionMultiValueConverter.cs
+++ View/Converters/GetFromCollectionMultiValueConverter.cs
@@ -1,5 +1,4 @@
 ﻿using Avalonia.Data.Converters;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -12,7 +11,7 @@ namespace View.Converters
         public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
             int index = 0;
-            if (values.Count<2)return null;
+            if (values.Count < 2) return null;
             if (values[0] is null) return null;
             if (!(values[1] is IEnumerable<object> list)) return null;
             if (!int.TryParse(values[0].ToString(), out index)) return null;
