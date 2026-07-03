ec3ee356b9e95577a42df79d23e5976da8d8351f
evgeny.muzyaev <evgeny.muzyaev@DESKTOP-M8Q0CE5>
2026-07-03 17:51:19 +0300

    feat: added control of 2nd nakopitel fix: deleted unused elements

---
+++ View/UserControls/Equipment/Nakopitel/NakopitelControl2.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace View.UserControls.Equipment.Nakopitel;
+
+public partial class NakopitelControl2 : UserControl
+{
+    public NakopitelControl2()
+    {
+        InitializeComponent();
+    }
+}
